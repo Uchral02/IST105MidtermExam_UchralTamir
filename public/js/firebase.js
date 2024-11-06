6a8855e889f35b62f0c9e270dd58c3579ce375ae
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2024-11-05 17:46:42 -0800

    first commit

---
+++ public/js/firebase.js
@@ -0,0 +1,13 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBqqTIe71I-oJqgBZD3zZ8OLasZLDgRv-8",
+    authDomain: "ist105midtermexamuchraltamir.firebaseapp.com",
+    projectId: "ist105midtermexamuchraltamir",
+    storageBucket: "ist105midtermexamuchraltamir.firebasestorage.app",
+    messagingSenderId: "649588766940",
+    appId: "1:649588766940:web:bcab65f64542495e401c19",
+    measurementId: "G-EFG931DVBR"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
